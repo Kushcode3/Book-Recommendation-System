3b82bb1199c40c36a35f8a2770903b27f0b8b00f
Kushcode3 <kush.bhardwajjj@gmail.com>
2024-11-12 01:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,47 @@
-from flask import Flask,render_template,request
+import streamlit as st
 import pickle
 import numpy as np
 
+
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
-books = pickle.load(open('books.pkl','rb'))
+books_with_ratings = pickle.load(open('books_with_ratings.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values),
-                           author=list(popular_df['Book-Author'].values),
-                           image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
-                           )
+def Top_popular_books(n):
+    top_n_books = popular_books_df.reset_index(drop=True).head(n)
 
-@app.route('/recommend')
-def recommend_ui():
-    return render_template('recommend.html')
+    return top_n_books
+    
 
-@app.route('/recommend_books',methods=['post'])
-def recommend():
-    user_input = request.form.get('user_input')
-    index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+def recommend(book_name):
+    # index fetch
+    if book_name in pt.index:
+        index = np.where(pt.index == book_name)[0][0]
 
-    data = []
-    for i in similar_items:
-        item = []
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        similar_items = sorted(list(enumerate(similarity_scores[index])),key=lambda x:x[1],reverse=True)[1:6]
 
-        data.append(item)
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books_with_ratings[books_with_ratings['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
 
-    print(data)
+            data.append(item)
 
-    return render_template('recommend.html',data=data)
+        return data
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    else:
+        print('Book not found')
+        
+st.title("Book Recommender")
+input = st.text_area("Enter book name")
+
+if st.button('Recommend'):
+
+    # Predict
+    result = recommend(book_name)
+    # Display
+    print(result)
